372bf3417f14fb219e4ce5fb9c1db4574a5db8ec
[evastream] <eva@evamedia.se>
2017-03-13 21:44:51 +0100

    Child-tema retrocrafts

---
+++ luca_retro1/retrocrafts/registration.php
@@ -0,0 +1,7 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/luca/retrocrafts',
+    __DIR__
+);
+/*Registration.php som Registrerar Child-temat retrocrafts*/
